940cf27b98dd4b56d8123c3b723aaa2679ad596a
Pragya-sable <141444273+Pragya-sable@users.noreply.github.com>
2025-03-04 20:36:57 +0530

    [Pragya] Add. Added Name Validation with Regex and Error handling in EmployeePayroll

--- Employee_Wages/EmployeePayroll.js
+++ Employee_Wages/EmployeePayroll.js
@@ -1,10 +1,24 @@
 class EmployeePayroll {
   constructor(id, name, salary, gender, startDate) {
     this.id = id;
-    this.name = name;
     this.salary = salary;
     this.gender = gender;
     this.startDate = new Date(startDate);
+    this.setName(name);
+  }
+
+  setName(name) {
+    try {
+      const nameRegex = /^[A-Z][a-zA-Z]{2,}$/;
+      if (!nameRegex.test(name)) {
+        throw new Error(
+          "Invalid Name! Name must start with a capital letter and have at least 3 characters."
+        );
+      }
+      this.name = name;
+    } catch (error) {
+      console.error(error.message);
+    }
   }
 
   getDetails = () =>
@@ -13,8 +27,10 @@ class EmployeePayroll {
     }, Start Date: ${this.startDate.toDateString()}`;
 }
 
-const emp1 = new EmployeePayroll(101, "Harsh", 5000, "Male", "2023-05-10");
+const emp1 = new EmployeePayroll(101, "Anand", 5000, "Male", "2023-05-10");
 const emp2 = new EmployeePayroll(102, "Pragya", 7000, "Female", "2022-11-15");
+const emp3 = new EmployeePayroll(103, "Harsh", 6000, "Male", "2024-01-20");
 
 console.log(emp1.getDetails());
 console.log(emp2.getDetails());
+console.log(emp3.getDetails());
